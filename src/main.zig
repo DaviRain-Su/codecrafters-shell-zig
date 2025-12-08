262bf28c457cef4eaf47e73864cbbeaaa7f0413b
DaviRain-Su <davirain.yin@gmail.com>
2025-12-08 13:14:27 +0800

    read on stdin

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,18 @@
 const std = @import("std");
 
+// define stdin
+var stdin_buffer: [4096]u8 = undefined;
+var stdin_readder = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+const stdin = &stdin_readder.interface;
+
+// define stdout
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
     // TODO: Uncomment the code below to pass the first stage
     try stdout.print("$ ", .{});
+
+    const command = try stdin.takeDelimiter('\n');
+    try stdout.print("{s}: command not found\n", .{command.?});
 }
