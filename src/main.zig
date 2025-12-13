bb4042b5f9d3510660e16cf42a983d42fe3ce77a
DaviRain-Su <davirain.yin@gmail.com>
2025-12-13 12:23:44 +0800

    add Command enum

--- src/main.zig
+++ src/main.zig
@@ -9,13 +9,23 @@ const stdin = &stdin_readder.interface;
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
+const Commands = enum {
+    exit,
+    notfound,
+};
+
 pub fn main() !void {
     while (true) {
-        const user_input = try prompt("$ ");
-        if (std.mem.eql(u8, user_input, "exit")) {
-            return std.process.exit(0);
+        const command = try prompt("$ ");
+        if (command.len == 0) {
+            continue;
+        } else {
+            const translated_command = std.meta.stringToEnum(Commands, command) orelse Commands.notfound;
+            switch (translated_command) {
+                Commands.exit => return std.process.exit(0),
+                Commands.notfound => try stdout.print("{s}: command not found\n", .{command}),
+            }
         }
-        try stdout.print("{s}: command not found\n", .{user_input});
     }
 }
 
