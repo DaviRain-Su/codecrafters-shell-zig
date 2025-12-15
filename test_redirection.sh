3c6df2c425b251b0062d074fe34f55702d93ec7b
DaviRain-Su <davirain.yin@gmail.com>
2025-12-15 16:03:14 +0800

    codecrafters submit [skip ci]

--- test_redirection.sh
+++ test_redirection.sh
@@ -4,4 +4,6 @@ echo "world" 1> test_echo_2.txt
 cat test_echo_2.txt
 ls -1 src > ls_out.txt
 cat ls_out.txt
-rm test_echo.txt test_echo_2.txt ls_out.txt
+echo "error_msg" 2> test_stderr.txt
+cat test_stderr.txt
+rm test_echo.txt test_echo_2.txt ls_out.txt test_stderr.txt
