03710acc6fbc0120ee66973c2235713c916a27b9
DaviRain-Su <davirain.yin@gmail.com>
2025-12-16 17:26:33 +0800

    codecrafters submit [skip ci]

--- test_redirection.sh
+++ test_redirection.sh
@@ -6,4 +6,9 @@ ls -1 src > ls_out.txt
 cat ls_out.txt
 echo "error_msg" 2> test_stderr.txt
 cat test_stderr.txt
-rm test_echo.txt test_echo_2.txt ls_out.txt test_stderr.txt
+echo "line1" > test_append.txt
+echo "line2" >> test_append.txt
+cat test_append.txt
+echo "new_append" >> test_append_new.txt
+cat test_append_new.txt
+rm test_echo.txt test_echo_2.txt ls_out.txt test_stderr.txt test_append.txt test_append_new.txt
